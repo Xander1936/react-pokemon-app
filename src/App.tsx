0283dfa4f9aebf2a891dff1899ce8e4020c88572
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-04-09 18:12:45 +0100

    Third commit: Show the Pokemon List -> App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,10 @@ import POKEMONS from './models/mock-pokemon';
 
 function App() {
   // Les Hooks sont toujours appeles a la racine et jamais dans une boucle.
-  // Hook d'Etat
+  // Hook d'Etat: useState()
   const [pokemons, setPokemons] = useState<Pokemon[]>([]);
 
-  // Hook d'Effet
+  // Hook d'Effet: useEffect(() => {},[ ])
   useEffect(() => {
     // Appelle la méthode du hook d'état setPokemons et on lui passe notre liste de pokémons à afficher.
     setPokemons(POKEMONS);
@@ -17,8 +17,29 @@ function App() {
 
   return (
       <div>
-        <h1>Pokédex</h1>
-        <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p>
+        {/* <p>Il y a {pokemons.length} pokémons dans le Pokédex.</p> */}
+        <h1 className='center'>Pokédex</h1>
+        {/* Affiche la liste des Pokémons */}
+        <div className="container">
+          <div className="row">
+            {pokemons.map(({id, name, picture, created}) =>(
+              <div className="col s6 m4" key={id}>
+                <div className="card horizontal">
+                  <div className="card-image">
+                    <img src={picture} alt={name} />
+                  </div>
+                  <div className="card-stacked">
+                    <div className="card-content">
+                      <p> {name} </p>
+                      {/* Récupère la date et la transforme en HTML  */}
+                      <p><small> {created.toString()} </small></p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
   )
 }
