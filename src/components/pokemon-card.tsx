87545607d9c92e3a23af1fb40d9a48d150e4a2f2
Xander1936 <alexandrehervemassodamambeleck@gmail.com>
2025-04-11 10:26:49 +0100

    Commit Number 6, Default  Props: borderColor added  to the PokemonCard + Some CSS fixes.

--- src/components/pokemon-card.tsx
+++ src/components/pokemon-card.tsx
@@ -1,19 +1,22 @@
 import { FunctionComponent } from 'react';
 import Pokemon from '../models/pokemon';
+import './pokemon-card.css'
 
 // Nouveau type props qui devra contenir un objet qui est un pokemon. 
 type Props = {
     pokemon: Pokemon;
+    // ? indique la variable borderColor est facultative
+    borderColor?: string
 }
 
 // On lie notre type Props avec notre propriété d'entrée.
 // Grâce à la généricité de TypeScript
-const PokemonCard: FunctionComponent<Props> = ({pokemon}) => {
+const PokemonCard: FunctionComponent<Props> = ({pokemon, borderColor = '#009688'}) => {
 
     return (
         // s12 signifie que sur les petits écrans (s: small), signifie qu'il va afficher 01 pokémon sur l'écran et sur les écrans moyens m-6 (m: medium), il va afficher 02 pokémons et chaque pokémon va prendre la moitié de l'écran: 1/2.
         <div className="col s12 m6">
-            <div className="card horizontal">
+            <div className="card horizontal" style={{ borderColor: borderColor }}>
                 <div className="card-image">
                     <img src={pokemon.picture} alt={pokemon.name} />
                 </div>
